423f6082e80ed436fb691b6b776fe446da1a0c68
David Nicholas <dnicholas11@hotmail.com>
2018-09-05 16:00:58 -0700

    Improvments Made... 4 Nodes

--- node.cpp
+++ node.cpp
@@ -101,19 +101,19 @@ void Node::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWid
     painter->setBrush(Qt::darkGray);
     painter->drawEllipse(-7, -7, 20, 20);
 
-    QRadialGradient gradient(-3, -3, 10);
-    if (option->state & QStyle::State_Sunken) {
-        gradient.setCenter(3, 3);
-        gradient.setFocalPoint(3, 3);
-        gradient.setColorAt(1, QColor(Qt::yellow).light(120));
-        gradient.setColorAt(0, QColor(Qt::darkYellow).light(120));
-    } else {
-        gradient.setColorAt(0, Qt::yellow);
-        gradient.setColorAt(1, Qt::darkYellow);
-    }
-    painter->setBrush(gradient);
+    int r = 255;
+    int g = 84;
+    int b = 0;
+    int alpha = 255;
 
-    painter->setPen(QPen(Qt::black, 0));
+    // Add Fermion and Boson Colors Here
+
+    QColor color = QColor(r,g,b,alpha);
+    QColor penColor = QColor(57,54,61,255);
+
+    painter->setBrush(color);
+
+    painter->setPen(QPen(penColor, 0));
     painter->drawEllipse(-10, -10, 20, 20);
 }
 
